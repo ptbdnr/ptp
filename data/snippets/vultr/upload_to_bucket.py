c2f0d72d1204fdad8e00f64279c25c4d2ab30e45
Peter <32688266+ptbdnr@users.noreply.github.com>
2025-04-11 23:18:15 +0100

    fix: access level

--- data/snippets/vultr/upload_to_bucket.py
+++ data/snippets/vultr/upload_to_bucket.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 from pathlib import Path
@@ -13,7 +14,7 @@ logger.addHandler(handler)
 
 dotenv.load_dotenv(".env.local")
 
-# constants
+# Constants
 HOSTNAME = os.environ.get("VULTR_OBJECT_STORAGE_HOSTNAME")
 ACCESS_KEY = os.environ.get("VULTR_OBJECT_STORAGE_ACCESS_KEY")
 SECRET_KEY = os.environ.get("VULTR_OBJECT_STORAGE_SECRET_KEY")
@@ -21,16 +22,14 @@ BUCKET_NAME = "ptpbcktdist01"
 
 FILEPATH = "./tmp_data/ptp-20250411t1610.mp4"
 OBJECT_KEY = "ptp20250411t1610.mp4"
+ACCESS_LEVEL = "public-read"
 
+# Print env variables
+logger.info("HOSTNAME: %s", HOSTNAME)
+logger.info("ACCESS_KEY len: %d", len(ACCESS_KEY))
+logger.info("SECRET_KEY len: %d", len(SECRET_KEY))
 
-# print env variables
-logger.info(f"HOSTNAME: {HOSTNAME}")
-logger.info(f"ACCESS_KEY: {ACCESS_KEY}")
-logger.info(f"SECRET_KEY: {SECRET_KEY}")
-logger.info(f"BUCKET_NAME: {BUCKET_NAME}")
-logger.info(f"FILEPATH: {FILEPATH}")
-logger.info(f"OBJECT_KEY: {OBJECT_KEY}")
-
+# Connect to Vultr Object Storage
 session = boto3.session.Session()
 client = session.client(
     "s3",
@@ -40,13 +39,28 @@ client = session.client(
     aws_secret_access_key=SECRET_KEY,
 )
 
+# List buckets
+bucket_list = client.list_buckets()
+logger.info("Buckets %s", json.dumps(client.list_buckets(), indent=2, default=str))
+
+# Check if bucket exists
+bucket_exists = False
+for bucket in bucket_list["Buckets"]:
+    if bucket["Name"] == BUCKET_NAME:
+        bucket_exists = True
+        break
+if not bucket_exists:
+    logger.info("Bucket %s does not exist", BUCKET_NAME)
+    # sys.exit(1)
+    client.create_bucket(Bucket=BUCKET_NAME, ACL=ACCESS_LEVEL)
+    logger.info("Bucket %s created", BUCKET_NAME)
+
+# Upload file to bucket
 with Path(FILEPATH).open("rb") as file:
     response = client.put_object(
         Bucket=BUCKET_NAME,
         Key=OBJECT_KEY,
         Body=file,
-        ACL="public-read",
-        ChecksumAlgorithm="sha1",
+        ACL=ACCESS_LEVEL,
 )
-
 logger.info(response)
