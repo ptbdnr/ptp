025804975450aee017a1d360524061d772c37fc4
Peter <32688266+ptbdnr@users.noreply.github.com>
2025-04-11 21:02:43 +0100

    feat: upload

--- data/snippets/vultr/upload_to_bucket.py
+++ data/snippets/vultr/upload_to_bucket.py
@@ -1,31 +1,52 @@
 import logging
+import os
+from pathlib import Path
 
 import boto3
+import dotenv
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+logger.addHandler(handler)
 
-PROFILE = "****"
-S3_BUCKET_NAME = "****"
-S3_TARGET_BUCKET_NAME = "****"
-REGION = "eu-west-1"
+dotenv.load_dotenv(".env.local")
 
-PREFIX = "****"
-KEY = "****"
-DOWNLOAD_DIR = "download"
+# constants
+HOSTNAME = os.environ.get("VULTR_OBJECT_STORAGE_HOSTNAME")
+ACCESS_KEY = os.environ.get("VULTR_OBJECT_STORAGE_ACCESS_KEY")
+SECRET_KEY = os.environ.get("VULTR_OBJECT_STORAGE_SECRET_KEY")
+BUCKET_NAME = "ptpbcktdist01"
 
-session = boto3.session.Session(profile_name=PROFILE)
-s3 = boto3.client("s3", REGION)
-s3_resource = boto3.resource("s3", REGION)
+FILEPATH = "./tmp_data/ptp-20250411t1610.mp4"
+OBJECT_KEY = "ptp20250411t1610.mp4"
 
-def upload_object(file_path, bucket_name, object_key):
-    try:
-        s3.upload_file(file_path, bucket_name, object_key)
-        logger.info(f"Successfully uploaded {file_path} to {bucket_name}/{object_key}")
-    except Exception as e:
-        logger.error(f"Failed to upload {file_path} to {bucket_name}/{object_key}: {e}")
 
-# Example usage
-file_path = "path/to/your/file.txt"
-object_key = "your/object/key.txt"
-upload_object(file_path, S3_BUCKET_NAME, object_key)
+# print env variables
+logger.info(f"HOSTNAME: {HOSTNAME}")
+logger.info(f"ACCESS_KEY: {ACCESS_KEY}")
+logger.info(f"SECRET_KEY: {SECRET_KEY}")
+logger.info(f"BUCKET_NAME: {BUCKET_NAME}")
+logger.info(f"FILEPATH: {FILEPATH}")
+logger.info(f"OBJECT_KEY: {OBJECT_KEY}")
+
+session = boto3.session.Session()
+client = session.client(
+    "s3",
+    region_name=HOSTNAME.split(".")[0],
+    endpoint_url="https://" + HOSTNAME,
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+)
+
+with Path(FILEPATH).open("rb") as file:
+    response = client.put_object(
+        Bucket=BUCKET_NAME,
+        Key=OBJECT_KEY,
+        Body=file,
+        ACL="public-read",
+        ChecksumAlgorithm="sha1",
+)
+
+logger.info(response)
