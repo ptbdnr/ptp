75df3fe3f1adfa52718415b7e2dc066de4d7fb96
Peter <32688266+ptbdnr@users.noreply.github.com>
2025-04-20 01:18:33 +0100

    feat: hosted_apii

--- back/prod/src/text_to_schema/ingredient_parser.py
+++ back/prod/src/text_to_schema/ingredient_parser.py
@@ -6,7 +6,6 @@ import os
 from textwrap import dedent
 from typing import Literal, Optional
 
-from mistralai import Mistral, ResponseFormat
 from openai import OpenAI
 
 from src.models.ingredients import Ingredient, Ingredients
@@ -17,33 +16,41 @@ handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
 logger.addHandler(handler)
 
+ENDPOINT_ROUTE = "/v1/chat/completions"
 
 class IngredientParser:
     """Generate meal images using AI and store them in Object Storage."""
 
-    provider: Literal["openai", "mistral"]
+    provider: Literal["openai", "mistral", "hosted_ai"]
     model_name: str
+    endpoint: str
     api_key: str
 
     def __init__(
         self,
         api_key: Optional[str] = None,
         model_name: Optional[str] = None,
-        provider: Optional[Literal["openai", "mistral"]] = None,
+        provider: Optional[Literal["openai", "mistral", "hosted_ai"]] = None,
     ) -> None:
         """Initialize the instance."""
         self.provider = provider or "mistral"
-        self.api_key = api_key or (
-            os.getenv("MISTRAL_API_KEY")
-            if self.provider == "mistral"
-            else os.getenv("OPENAI_API_KEY")
-        )
-        self.model_name = model_name or (
-            os.getenv("MISTRAL_MODEL_NAME")
-            if self.provider == "mistral"
-            else os.getenv("OPENAI_MODEL_NAME")
-        )
         logger.debug("MealGenerator initialized with provider: %s", self.provider)
+        if self.provider == "mistral":
+            self.model_name = model_name or os.getenv("MISTRAL_MODEL_NAME")
+            self.api_key = api_key or os.getenv("MISTRAL_API_KEY")
+        elif self.provider == "openai":
+            self.model_name = model_name or os.getenv("OPENAI_MODEL_NAME")
+            self.api_key = api_key or os.getenv("OPENAI_API_KEY")
+        elif self.provider == "hosted_ai":
+            self.model_name = model_name or os.getenv("HOSTED_AI_MODEL_NAME")
+            endpoint_ip = os.getenv("HOSTED_AI_IP")
+            endpoint_port = os.getenv("HOSTED_AI_PORT")
+            self.endpoint = f"http://{endpoint_ip}:{endpoint_port}/{ENDPOINT_ROUTE}"
+            logger.debug("Endpoint: %s", self.endpoint)
+        else:
+            msg = f"Unknown provider: {self.provider}"
+            logger.error(msg)
+            raise ValueError(msg)
         logger.debug("Model name: %s", self.model_name)
 
     def text_to_ingredients(
@@ -137,3 +144,32 @@ class IngredientParser:
         logger.debug("OpenAI response: %s", response)
 
         return json.loads(response.output_text)
+
+    def _hosted_ai(
+            self,
+            system_msg: str,
+            prompt: str,
+            schema: dict,
+    ) -> dict:
+        """Call mistral API."""
+        model_name = self.model_name
+        logger.debug("Using model: %s with respose format: %s", model_name, schema)
+        response = requests.post(
+            f"{self.endpoint}/v1/chat/completions",
+            headers={
+                "Content-Type": "application/json",
+            },
+            json={
+                "model": HOSTED_AI_MODEL_NAME,
+                "messages": [
+                    {"role": "system", "content": system_msg},
+                    {"role": "user", "content": prompt},
+                ],
+                "max_tokens": 2000,
+            },
+            timeout=300,
+        )
+        logger.debug("response: %s", response.text)
+        chat_response = response.json().get("choices", [{}])[0].get("message", {}).get("content", "")
+        logger.debug(chat_response)
+        return json.loads(chat_response)
