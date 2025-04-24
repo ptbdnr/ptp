9152666df1f1f2b79e2fd5672a80790c119f3ce7
Peter <32688266+ptbdnr@users.noreply.github.com>
2025-04-24 22:31:06 +0100

    fix: mock then db

--- front/nextjs_ts/src/app/meals/[slug]/page.tsx
+++ front/nextjs_ts/src/app/meals/[slug]/page.tsx
@@ -10,23 +10,27 @@ import styles from './meals.module.css';
 import { mockupMeals } from '@/data/meals';
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
-  // const router = useRouter()
   const { slug } = await params;
   console.log('slug', slug);
-  // Inside your component function:
-  const headersList = await headers();
-  const host = headersList.get('host') || 'localhost:3000';
-  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
-  const baseUrl = `${protocol}://${host}`;
-  // const response = await fetch(`/api/meals?slug=${slug}`);
-  const response = await fetch(`${baseUrl}/api/meals`);
-  if (!response.ok) {
-    throw new Error('Failed to fetch meal data');
-  }
-  const mealData = await response.json();
-  const meal = mockupMeals.find(
-    (meal) => meal.id === slug) || mealData.meals.find((meal: Meal) => meal.id === slug);
+  let meal = mockupMeals.find((meal) => meal.id === slug)
   
+  if (!meal) {
+    console.log("query database");
+    const headersList = await headers();
+    const host = headersList.get('host') || 'localhost:3000';
+    const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+    const baseUrl = `${protocol}://${host}`;
+    const response = await fetch(`${baseUrl}/api/meals`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch meal data');
+    }
+    const mealData = await response.json();
+    meal = mealData.meals.find((meal: Meal) => meal.id === slug);
+  }
+  
+  
+
+
   return (
     <MealsLayout>
       <div className={styles.content}>
